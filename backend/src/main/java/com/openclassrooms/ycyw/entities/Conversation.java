c9c3e391626d2be82a7dc4dcd074933eddd37bbb
clarabernadou <clarabernadou56690@gmail.com>
2025-01-31 14:43:19 +0100

    [add] - create entity

---
+++ backend/src/main/java/com/openclassrooms/ycyw/entities/Conversation.java
@@ -0,0 +1,52 @@
+package com.openclassrooms.ycyw.entities;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "conversations")
+public class Conversation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user1_id", nullable = false)
+    private Auth user1Id;
+
+    @ManyToOne
+    @JoinColumn(name = "user2_id", nullable = false)
+    private Auth user2Id;
+
+    @Column(updatable = false, name = "created_at")
+    private LocalDate createdAt;
+
+    @Column(updatable = true, name = "updated_at")
+    private LocalDate updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDate.now();
+        updatedAt = LocalDate.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDate.now();
+    }
+
+    public Conversation() {}
+}
