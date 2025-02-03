c84f4fc654b8b543e9d675b2dbb03ff8a2944874
clarabernadou <clarabernadou56690@gmail.com>
2025-02-03 22:39:00 +0100

    [add] - create controller

---
+++ backend/src/main/java/com/openclassrooms/ycyw/controllers/MessageController.java
@@ -0,0 +1,41 @@
+package com.openclassrooms.ycyw.controllers;
+
+import java.security.Principal;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.openclassrooms.ycyw.dto.MessageDTO;
+import com.openclassrooms.ycyw.entities.Message;
+import com.openclassrooms.ycyw.services.interfaces.MessageService;
+
+@RestController
+@RequestMapping("/api/auth/conversation")
+public class MessageController {
+    private final MessageService messageService;
+
+    @Autowired
+    public MessageController(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    @PostMapping("/{conversationId}/message")
+    public ResponseEntity<String> sendMessage(@Valid @RequestBody MessageDTO messageDTO, @PathVariable Long conversationId, Principal principalUser) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(messageService.sendMessage(messageDTO, conversationId, principalUser).orElseThrow(() -> new RuntimeException("Message failed")));
+    }
+
+    @GetMapping("/{conversationId}/messages")
+    public ResponseEntity<List<Message>> getAllMessagesOfConversation(@PathVariable Long conversationId) {
+        return ResponseEntity.status(HttpStatus.OK).body(messageService.getAllMessagesByConversationId(conversationId));
+    }
+}
